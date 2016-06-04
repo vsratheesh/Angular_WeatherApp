d7be1838de392ab054b2667e0ed2712626d39875
vsratheesh <vsratheesh95@gmail.com>
2016-06-04 19:26:51 +0530

    created Forecast

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-var angularApp = angular.module('weatherApp',  ['ngResource','ngRoute']);
+var angularApp = angular.module('weatherApp', ['ngResource','ngRoute']);
 
 angularApp.config(function ($routeProvider){
-  $routeProvider
-  .when ('/',{
-    templateUrl: 'templates/current.html',
-    controller: 'HomeController'
-  })
+ $routeProvider
+ .when ('/',{
+   templateUrl: 'templates/current.html',
+   controller: 'HomeController'
+ })
 
-
-
-angularApp.controller("HomeController",function($resource){
-	var vm=this;
-
-	vm.getCityTemperature = function(){
-		console.log(vm.cityName);
-		var weatherResource = $resource('http://api.openweathermap.org/data/2.5/weather',
-			{q:vm.cityName,appid:'27d43832d2a4adcb97fcbfa23db130aa'});
-		vm.weatherResponse = weatherResource.get();
-		vm.weather.$promise.then(function(data){
-      vm.weatherResponse = data;
-      vm.weatherResponse.main.temp -= 273;
-      console.log(vm.weatherResponse)
-    }, function(error){
-      console.log(error);
-    });
-   }
+ .when ('/forecast',{
+   templateUrl: 'templates/forecast.html',
+   controller: 'ForecastController'
+ })
 });
 
 
+angularApp.controller("HomeController",function($resource){
+ var vm=this;
+ vm.getCityTemperature = function(){
+   console.log(vm.cityName);
+   var weatherResource = $resource('http://api.openweathermap.org/data/2.5/weather',
+     {q:vm.cityName,appid:'850afafc23984bdc9affaf4257bf7b8b'});
+   vm.weatherResponse = weatherResource.get();
+   console.log(vm.weatherResponse);
+  }
+});
+
+angularApp.controller("ForecastController",function($resource){
+ var vm=this;
+ vm.getCityForecast = function(){
+   console.log(vm.cityName);
+   var forecastResource = $resource('http://api.openweathermap.org/data/2.5/forecast',
+     {q:vm.cityName,appid:'850afafc23984bdc9affaf4257bf7b8b',cnt:10});
+   vm.forecastResponse = forecastResource.get();
+   console.log(vm.forecastResponse);
+  }
+
+});
