6e49158e5060bd3a945eba28526a3c8f246f2793
vsratheesh <vsratheesh95@gmail.com>
2016-06-04 17:54:36 +0530

    created templates

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var angularApp = angular.module('weatherApp', ['ngResource']);
+var angularApp = angular.module('weatherApp',  ['ngResource','ngRoute']);
+
+angularApp.config(function ($routeProvider){
+  $routeProvider
+  .when ('/',{
+    templateUrl: 'templates/current.html',
+    controller: 'HomeController'
+  })
+
+
+
 angularApp.controller("HomeController",function($resource){
 	var vm=this;
 
@@ -7,6 +17,14 @@ angularApp.controller("HomeController",function($resource){
 		var weatherResource = $resource('http://api.openweathermap.org/data/2.5/weather',
 			{q:vm.cityName,appid:'27d43832d2a4adcb97fcbfa23db130aa'});
 		vm.weatherResponse = weatherResource.get();
-		console.log(vm.weatherResponse);
-	}
+		vm.weather.$promise.then(function(data){
+      vm.weatherResponse = data;
+      vm.weatherResponse.main.temp -= 273;
+      console.log(vm.weatherResponse)
+    }, function(error){
+      console.log(error);
+    });
+   }
 });
+
+
